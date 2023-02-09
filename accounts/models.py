29e139d8166c96de628765958cd9d21ce816f29a
svalench <chitsalex@gmail.com>
2023-02-09 23:16:38 +0300

    pep

--- accounts/models.py
+++ accounts/models.py
@@ -2,11 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-
-
 class CustomUser(AbstractUser):
     """Модель пользователей"""
-
 
     def __str__(self):
         return self.username
