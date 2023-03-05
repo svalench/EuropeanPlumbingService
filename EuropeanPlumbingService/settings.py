90f48d4628966086a41f469e85843de9497a615d
svalench <chitsalex@gmail.com>
2023-03-05 14:17:08 +0300

    CSRF_TRUSTED_ORIGINS

--- EuropeanPlumbingService/settings.py
+++ EuropeanPlumbingService/settings.py
@@ -160,4 +160,4 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://192.168.56.1:3000",
 ]
-CSRF_TRUSTED_ORIGINS =[HOSTNAME]
+CSRF_TRUSTED_ORIGINS =[HOSTNAME, 'https://api.valenchits.com']
