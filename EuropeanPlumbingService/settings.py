04055273d378e61130853c19f2b930ec4fe72a02
svalench <chitsalex@gmail.com>
2023-08-01 22:31:55 +0300

    swagger

--- EuropeanPlumbingService/settings.py
+++ EuropeanPlumbingService/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework.authtoken',
     'rest_framework',
     'oauth2_provider',
--- EuropeanPlumbingService/urls.py
+++ EuropeanPlumbingService/urls.py
@@ -20,6 +20,24 @@ from rest_framework import routers
 
 from accounts.viewset import UserViewSet, UserAuthToken, ClientsViewSet, UsersRolesViewSet, RegisterView
 from kitstructure.viewset import AppObjetSet, ApiOfAppViewSet
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -36,4 +54,8 @@ urlpatterns = [
     path('user/', include('accounts.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('oauth/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
