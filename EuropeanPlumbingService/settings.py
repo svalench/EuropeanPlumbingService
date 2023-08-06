f73412c0673a3d8eb5ba778ade59e7224dc557b2
svalench <chitsalex@gmail.com>
2023-08-06 18:16:41 +0300

    add prod host

--- EuropeanPlumbingService/settings.py
+++ EuropeanPlumbingService/settings.py
@@ -164,6 +164,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://localhost:3000",
+    "http://localhost:3001",
     "http://127.0.0.1:8000",
     "https://site.valenchits.com"
 ]
