25c84d1b149871cbb2a94622c81410cdefd33c23
svalench <chitsalex@gmail.com>
2023-08-07 18:58:30 +0300

    add headers

--- kitstructure/utils.py
+++ kitstructure/utils.py
@@ -2,7 +2,8 @@ import requests
 from django.conf import settings
 
 
-def send_request_to_api_kit_service(uri: str=None, method='GET', data: dict = None, *args, **kwargs):
+def send_request_to_api_kit_service(uri: str = None, method: str = 'GET',
+                                    data: dict = None, *args, **kwargs) -> requests.Response:
     session = requests.Session()
     header = {
         'Authorization': 'Bearer ggfgfghgggf'
@@ -10,6 +11,8 @@ def send_request_to_api_kit_service(uri: str=None, method='GET', data: dict = No
     kwargs['headers'] = {**kwargs['headers'], **header} if kwargs.get('headers') else header
     session.headers.update(kwargs['headers'])
     if method == 'POST' and data:
-        kwargs['json'] = data
+        #kwargs['json'] = data
         kwargs['data'] = data
-    return session.request(method=method, url=f'{settings.API_KIT_SERVICE}/{uri}', **kwargs)
+    response = session.request(method=method, url=f'{settings.API_KIT_SERVICE}/{uri}', **kwargs)
+    print(response.content)
+    return response
