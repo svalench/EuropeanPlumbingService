783f622b06a2fcf7830f564ac6d3a0da522d2ba4
svalench <chitsalex@gmail.com>
2023-08-07 21:59:25 +0300

    fix request

--- kitstructure/utils.py
+++ kitstructure/utils.py
@@ -5,12 +5,11 @@ from django.conf import settings
 def send_request_to_api_kit_service(uri: str = None, method: str = 'GET',
                                     data: dict = None, *args, **kwargs) -> requests.Response:
     session = requests.Session()
-    header = {'Authorization': 'Bearer ggfgfghgggf'}
+    header = {'Authorization': 'Bearer ggfgfghgggf', "Content-Type": "application/json; charset=utf-8"}
     kwargs['headers'] = {**kwargs['headers'], **header} if kwargs.get('headers') else header
     session.headers.update(kwargs['headers'])
     if method == 'POST' and data:
-        #kwargs['json'] = data
-        kwargs['data'] = data
+        kwargs['json'] = data
     print(kwargs, kwargs)
     response = session.request(method=method, url=f'{settings.API_KIT_SERVICE}/{uri}', **kwargs)
     print(response.content)
