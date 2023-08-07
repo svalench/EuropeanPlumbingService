505dc7c212626ddb539bfc7a7272575a80d77c1c
svalench <chitsalex@gmail.com>
2023-08-07 18:44:06 +0300

    logs

--- kitstructure/utils.py
+++ kitstructure/utils.py
@@ -11,4 +11,5 @@ def send_request_to_api_kit_service(uri: str=None, method='GET', data: dict = No
     session.headers.update(header)
     if method == 'POST' and data:
         kwargs['json'] = data
+        kwargs['data'] = data
     return session.request(method=method, url=f'{settings.API_KIT_SERVICE}/{uri}', **kwargs)
